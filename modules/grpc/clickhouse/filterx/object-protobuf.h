e47b3f7709db6bc6fd2902526340e3a155da3e9d
shifter <shifter@axoflow.com>
2025-05-09 12:10:52 +0200

    add function proto to clickhouse

---
+++ modules/grpc/clickhouse/filterx/object-protobuf.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2025 Axoflow
+ * Copyright (c) 2025 shifter
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef OBJECT_PROTOBUFMESSAGE_H
+#define OBJECT_PROTOBUFMESSAGE_H
+
+#include "syslog-ng.h"
+
+#include "compat/cpp-start.h"
+#include "filterx/filterx-object.h"
+#include "filterx/expr-function.h"
+
+FILTERX_FUNCTION_DECLARE(protobuf_message);
+FilterXExpr *filterx_protobuf_message_new_from_args(FilterXFunctionArgs *args, GError **error);
+
+// FILTERX_SIMPLE_FUNCTION_DECLARE(protobuf_message);
+// FilterXObject *filterx_protobuf_message_new_from_args(FilterXExpr *s, FilterXObject *args[], gsize args_len);
+
+// static inline FilterXObject *
+// filterx_protobuf_message_new(void)
+// {
+//   return filterx_protobuf_message_new_from_args(NULL, NULL, 0);
+// }
+
+FILTERX_DECLARE_TYPE(protobuf_message);
+
+static inline void
+protobuf_filterx_objects_global_init(void)
+{
+  static gboolean initialized = FALSE;
+
+  if (!initialized)
+    {
+      filterx_type_init(&FILTERX_TYPE_NAME(protobuf_message));
+      initialized = TRUE;
+    }
+}
+
+#include "compat/cpp-end.h"
+
+#endif
